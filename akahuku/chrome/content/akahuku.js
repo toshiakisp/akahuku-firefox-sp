d5bb806686ff370d93ec7e87d26263fe6e9f1340
toshiakisp <toshiaki.sp@mail>
2012-08-05 11:34:16 +0900

    add ipv6 and some boards into apply targets

--- akahuku/chrome/content/akahuku.js
+++ akahuku/chrome/content/akahuku.js
@@ -341,9 +341,9 @@ var Akahuku = {
    */
   getNeedApply : function (targetDocument, href) {
     if (href.match
-        (/^http:\/\/([^\/]+\/)?(tmp|up|img|cgi|zip|dat|may|nov|jun|dec)\.2chan\.net(:[0-9]+)?\/([^\/]+)\//)
+        (/^http:\/\/([^\/]+\/)?(tmp|up|img|cgi|zip|dat|may|nov|jun|dec|ipv6)\.2chan\.net(:[0-9]+)?\/([^\/]+)\//)
         || href.match
-        (/^http:\/\/([^\/]+\/)?(www)\.2chan\.net(:[0-9]+)?\/(h|oe|b|30|31|junbi)\//)) {
+        (/^http:\/\/([^\/]+\/)?(www)\.2chan\.net(:[0-9]+)?\/(h|oe|b|30|31|51|junbi)\//)) {
       /* ふたばの板 */
       return true;
     }
@@ -354,9 +354,9 @@ var Akahuku = {
           || p.type == "filecache") {
         var href2 = p.original;
         if (href2.match
-            (/^http:\/\/([^\/]+\/)?(tmp|up|img|cgi|zip|dat|may|nov|jun|dec)\.2chan\.net(:[0-9]+)?\/([^\/]+)\//)
+            (/^http:\/\/([^\/]+\/)?(tmp|up|img|cgi|zip|dat|may|nov|jun|dec|ipv6)\.2chan\.net(:[0-9]+)?\/([^\/]+)\//)
             || href2.match
-            (/^http:\/\/([^\/]+\/)?(www)\.2chan\.net(:[0-9]+)?\/(h|oe|b|30|31)\//)) {
+            (/^http:\/\/([^\/]+\/)?(www)\.2chan\.net(:[0-9]+)?\/(h|oe|b|30|31|51|junbi)\//)) {
           /* ふたばの板のキャッシュ */
           return true;
         }
