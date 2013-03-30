8027e6d691227bdfadd52ea72bac5c0020ef564b
toshiakisp <toshiaki.sp@mail>
2013-03-31 01:15:16 +0900

    Improve post status behaviors when post closed
    * Hide post error when form is closed
    * Change response error detail togglable by button

--- akahuku/chrome/content/mod/arAkahukuPostForm.js
+++ akahuku/chrome/content/mod/arAkahukuPostForm.js
@@ -1559,12 +1559,15 @@ var arAkahukuPostForm = {
    */
   onResponseErrorClick : function (event) {
     var targetDocument = event.target.ownerDocument;
-    var param
-    = Akahuku.getDocumentParam (targetDocument).postform_param;
         
     var n = targetDocument.getElementById ("akahuku_response_error");
     if (n) {
-      n.style.display = "block";
+      if (n.style.display !== "none") {
+        n.style.display = "none";
+      }
+      else {
+        n.style.display = "block";
+      }
     }
   },
     
@@ -1962,6 +1965,10 @@ var arAkahukuPostForm = {
     var commentbox
     = targetDocument.getElementById ("akahuku_commentbox")
     || targetDocument.getElementById ("ftxa");
+    var status1
+    = targetDocument.getElementById ("akahuku_reply_status");
+    var status2
+    = targetDocument.getElementById ("akahuku_reply_status2");
     
     if (hide) {
       postform.style.overflow = "hidden";
@@ -1972,6 +1979,12 @@ var arAkahukuPostForm = {
       postform.style.height = "0px";
       postform.style.visibility = "hidden";
       postform.style.padding = "0px";
+      if (status1) {
+        status1.style.display = "none";
+      }
+      if (status2) {
+        status2.style.display = "none";
+      }
     }
     else {
       postform.style.overflow = "";
@@ -1987,6 +2000,12 @@ var arAkahukuPostForm = {
       postform.style.padding = "";
       postform.style.display = "block";
       postform.style.visibility = "visible";
+      if (status1) {
+        status1.style.display = "block";
+      }
+      if (status2) {
+        status2.style.display = "block";
+      }
     }
   },
     
@@ -2820,10 +2839,6 @@ var arAkahukuPostForm = {
       var postformFooterIcon
       = targetDocument
       .getElementById ("akahuku_floatpostform_footer_icon");
-      var status
-      = targetDocument.getElementById ("akahuku_reply_status");
-      var status2
-      = targetDocument.getElementById ("akahuku_reply_status2");
       var close
       = targetDocument.getElementById ("akahuku_floatpostform_close");
       var commentbox
@@ -2867,12 +2882,6 @@ var arAkahukuPostForm = {
           if (postformFooterContent) {
             postformFooterContent.style.display = "none";
           }
-          if (status) {
-            status.style.display = "none";
-          }
-          if (status2) {
-            status2.style.display = "none";
-          }
           if (postformContainer) {
             postformContainer.style.width = "27px";
           }
@@ -2889,12 +2898,6 @@ var arAkahukuPostForm = {
           }
           if (postformFooterContent) {
             postformFooterContent.style.display = "";
-          }
-          if (status) {
-            status.style.display = "block";
-          }
-          if (status2) {
-            status2.style.display = "block";
           }
           if (postformContainer) {
             postformContainer.style.width
