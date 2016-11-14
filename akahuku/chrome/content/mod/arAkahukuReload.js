daa828b6a4cac13a29a075212ae813e16b3392ae
toshiakisp <toshiaki.sp@mail>
2016-11-14 20:16:22 +0900

    Store actual HTTP protocol version in reloaded cache

--- akahuku/chrome/content/mod/arAkahukuReload.js
+++ akahuku/chrome/content/mod/arAkahukuReload.js
@@ -709,8 +709,26 @@ arAkahukuReloadParam.prototype = {
         = httpChannel.responseStatus;
             
       // キャッシュ書込用レスポンスヘッダーの作成
-      // 本当の HTTP レスポンスを得るには少し手順が必要なので仮定して
-      responseHead = "HTTP/1.1 "
+      var protocolVersion = "1.1";
+      if ("protocolVersion" in request) {
+        // requires Firefox 45
+        switch (request.protocolVersion) {
+          case "h2":
+            protocolVersion = "2.0";
+            break;
+          case "http/1.1":
+            protocolVersion = "1.1";
+            break;
+          case "http/1.0":
+            protocolVersion = "1.0";
+            break;
+          default:
+            Akahuku.debug.warn ("unknwon protocolVersion "
+                + request.protocolVersion);
+            protocolVersion = "1.1";
+        }
+      }
+      responseHead = "HTTP/" + protocolVersion + " "
         + httpChannel.responseStatus + " "
         + httpChannel.responseStatusText + "\r\n";
       var visitor = {
